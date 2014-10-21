824c476836d7380ff5f44ad62c819e2adf1314f0
Robert Kaye <rob@musicbrainz.org>
2014-10-21 14:30:55 +0200

    Added a rudimentary diff

---
+++ tools/ex-json-diff.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+from math import fabs
+
+FLOAT_DIFF_THRESHOLD = 0.0001
+
+def traverse(path, jdata1, jdata2):
+    if type(jdata1) is dict:
+        for k in sorted(jdata1.keys()):
+            path.append(k)
+            if not jdata2.has_key(k):
+                print "json2 is missing %s" % (":".join(path))
+                sys.exit(-1)
+
+            traverse(path, jdata1[k], jdata2[k])
+            path.pop()
+        return
+
+    if type(jdata1) is list:
+        for i, item in enumerate(jdata1):
+            path.append("%d" % i)
+            try:
+                dummy = jdata2[i]
+            except IndexError:
+                print "json2 is missing %s" % (":".join(path))
+                sys.exit(-1)
+            traverse(path, item, jdata2[i])
+            path.pop()
+        return
+
+    if type(jdata1) is float:
+        d = fabs(jdata1 - jdata2)
+        if d > FLOAT_DIFF_THRESHOLD:
+            print "%s : '%s' vs '%s' (diff %f)" % (":".join(path), repr(jdata1), repr(jdata2), d)
+    elif type(jdata1) is int:
+        if jdata1 != jdata2:
+            print "%s : '%d' vs '%d'" % (":".join(path), jdata1, jdata2)
+    elif repr(jdata1) != repr(jdata2):
+        print "%s : '%s' vs '%s'" % (":".join(path), repr(jdata1), repr(jdata2))
+
+if len(sys.argv) < 3:
+    print "Usage: %s <json file 1> <json file 2>" % sys.argv[0]
+    sys.exit(-1)
+
+try:    
+    f1 = open(sys.argv[1], "r")
+    json1 = f1.read()
+    f1.close()
+except IOError, e:
+    print "Cannot open %s: %s" % (sys.argv[1], e)
+    sys.exit(-1)
+
+try:    
+    f2 = open(sys.argv[2], "r")
+    json2 = f2.read()
+    f2.close()
+except IOError, e:
+    print "Cannot open %s: %s" % (sys.argv[1], e)
+    sys.exit(-1)
+
+try:
+    jdata1 = json.loads(json1)
+except ValueError, e:
+    print "Cannot parse JSON 1: %s" % e
+    sys.exit(-1)
+
+try:
+    jdata2 = json.loads(json2)
+except ValueError, e:
+    print "Cannot parse JSON 2: %s" % e
+    sys.exit(-1)
+
+traverse([], jdata1, jdata2)
